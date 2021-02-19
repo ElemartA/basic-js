99b9beb8fd067032844fb645e38fd9733934edc0
ElemartA <info88881@gmail.com>
2021-02-19 11:01:41 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+  let newArray = [];
+  if(array != null){
+  for (let i=0; i<array.length; i++){
+    if(typeof array[i] === 'string'){
+      let firstLetters = array[i].trim()[0].toUpperCase();
+      newArray.push(firstLetters);
+  }
+}
+}else{
+  return false;
+}
+  return newArray.sort().join('');
 };
+
