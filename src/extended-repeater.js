e865bb943efc30144c1c3b81e0f3ad9f76ea3a06
ElemartA <info88881@gmail.com>
2021-02-19 11:10:49 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let string = String(str);
+  if (options.separator === undefined) {
+    options.separator = '+'
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|'
+  }
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1
+  }
+  let adding = (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes);
+  let additionSeparatorLength = options.additionSeparator.length;
+  let cutStr = adding.substring(0, adding.length - additionSeparatorLength);
+  let newStr = (string + cutStr);
+  let separatorLength = options.separator.length;
+  let result = (newStr + options.separator).repeat(options.repeatTimes);
+  let finishResult = result.substring(0, result.length - separatorLength);
+  return finishResult;
 };
-  
