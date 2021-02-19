37749f4f2a00b6837146e392b6256ef3af22e4dc
ElemartA <info88881@gmail.com>
2021-02-19 11:18:57 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let arrN = [...arr]
+  if (arrN === []) return [];
+
+  if (!Array.isArray(arrN)) {
+    throw new Error('error')
+  }
+  for (let i = 0; i < arrN.length; i++) {
+    switch (arrN[i]) {
+      case '--discard-next':
+
+        if (i === arrN.length - 1) {
+          arrN.splice(i, 1);
+          break;
+        }
+        else if (arrN[i + 2] === '--discard-prev' || arrN[i + 2] === '--double-prev') {
+          arrN.splice(i, 3);
+          break;
+        }
+        arrN.splice(i, 2);
+        i = i - 1;
+        break;
+      case '--discard-prev':
+        if (i === 0) {
+          arrN.splice(i, 1);
+          break;
+        }
+        arrN.splice(i - 1, 2);
+        i = i - 1;
+        break;
+      case '--double-next':
+        if (i < arrN.length - 1) {
+          arrN.splice(i, 1, arrN[i + 1]);
+          break;
+        } else if (i === arrN.length - 1) {
+          arrN.splice(i, 1);
+          break;
+        }
+      case '--double-prev':
+        if (i === 0) {
+          arrN.splice(i, 1);
+          break;
+        } else if (i > 0) {
+          arrN.splice(i, 1, arrN[i - 1]);
+          break;
+        }
+    }
+  } return arrN;
 };
